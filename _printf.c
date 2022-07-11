f140e15f5f183053772a59116444c1ac918aff7c
teslynnchristians <teslynnchr02@gmail.com>
2022-07-11 10:00:17 -0700

    Task 0 in printf

--- _printf.c
+++ _printf.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
+ * _printf.c - Receives the main string and all the necessary parameters to
  * print a formated string
  * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-int printed_chars;
-conver_t f_list[] = {
+pt_fmt pt_format[0] = {
 {"c", print_char},
 {"s", print_string},
 {"%", print_percent},
